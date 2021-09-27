c611864f2e1ea3e3b65d57b60ccaca5b69c4a58a
PeterJakobs1 <peter.bj@live.no>
2021-09-27 16:44:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Forelesning 4: Http server
+
+HTTP request ser ut
+
+
+## Repeat av lecture 2
+
+* [x] Maven
+* [x] Github Actions
+* [x] Working branch
+* [x] Pull request
+* [x] Test report
+
+## Repeat av lecture 3: Socket, HTTP
+
+* [x] Socket connects server
+* [x] Status code
+* [x] Header fields
+* [x] Content-length
+* [x] Message body
+
+## TODAY: HttpServer
+
+* [x] HttpServer should respond with 404
+* [x] HttpServer should include request target in 404
+* [x] Return a static content for /hello
+* [ ] Content-type
+* [ ] Return HTML file from disk
+* [ ] Return <form>
+* [ ] Process GET request for form
+* [ ] Process POST request from form
---
+++ src/main/java/no/kristiania/http/HttpServer.java
@@ -0,0 +1,94 @@
+package no.kristiania.http;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class HttpServer {
+
+    private final ServerSocket serverSocket;
+    private Path rootDirectory;
+
+    public HttpServer(int serverPort) throws IOException {
+        serverSocket = new ServerSocket(serverPort);
+
+        new Thread(this::handleClients).start();
+    }
+
+    private void handleClients() {
+        try {
+            Socket clientSocket = serverSocket.accept();
+
+            String[] requestLine = HttpClient.readLine(clientSocket).split(" ");
+            String requestTarget = requestLine[1];
+
+            int questionPos = requestTarget.indexOf('?');
+            String fileTarget;
+            String query = null;
+            if (questionPos != -1) {
+                fileTarget = requestTarget.substring(0, questionPos);
+                query = requestTarget.substring(questionPos + 1);
+            } else {
+                fileTarget = requestTarget;
+            }
+
+            if (fileTarget.equals("/hello")) {
+                String yourName = "world";
+                if (query != null) {
+                    yourName = query.split("=")[1];
+                }
+                String responseText = "<p>Hello " + yourName + "</p>";
+
+                String response = "HTTP/1.1 200 OK\r\n" +
+                        "Content-Length: " + responseText.length() + "\r\n" +
+                        "Content-Type: text/html\r\n" +
+                        "\r\n" +
+                        responseText;
+                clientSocket.getOutputStream().write(response.getBytes());
+            } else {
+                if (rootDirectory != null && Files.exists(rootDirectory.resolve(fileTarget.substring(1)))) {
+                    String responseText = Files.readString(rootDirectory.resolve(fileTarget.substring(1)));
+
+                    String contentType = "text/plain";
+                    if (requestTarget.endsWith(".html")) {
+                        contentType = "text/html";
+                    }
+                    String response = "HTTP/1.1 200 OK\r\n" +
+                            "Content-Length: " + responseText.length() + "\r\n" +
+                            "Content-Type: " + contentType + "\r\n" +
+                            "\r\n" +
+                            responseText;
+                    clientSocket.getOutputStream().write(response.getBytes());
+                    return;
+                }
+
+
+                String responseText = "File not found: " + requestTarget;
+
+                String response = "HTTP/1.1 404 Not found\r\n" +
+                        "Content-Length: " + responseText.length() + "\r\n" +
+                        "\r\n" +
+                        responseText;
+                clientSocket.getOutputStream().write(response.getBytes());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        HttpServer httpServer = new HttpServer(1962);
+        httpServer.setRoot(Paths.get("."));
+    }
+
+    public int getPort() {
+        return serverSocket.getLocalPort();
+    }
+
+    public void setRoot(Path rootDirectory) {
+        this.rootDirectory = rootDirectory;
+    }
+}
